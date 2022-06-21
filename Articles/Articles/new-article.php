dcf5d968285630478e201ecbd54c67198ee8f220
rutkowsm <37616390+rutkowsm@users.noreply.github.com>
2022-06-22 01:28:45 +0200

    funkcje + edycja wpisów

---
+++ Articles/Articles/new-article.php
@@ -0,0 +1,71 @@
+<?php
+
+require 'includes/db_connection.php';
+require 'includes/article-functions.php';
+require 'includes/url.php';
+
+$title = '';
+$content = '';
+$published_at = '';
+
+  if ($_SERVER["REQUEST_METHOD"] == "POST") {
+
+
+    $title = $_POST['title'];
+    $content = $_POST['content'];
+    $published_at = $_POST['published_at'];
+
+    $errors = validateArticle($title, $content, $published_at);
+
+
+    if (empty($errors)){
+
+
+    $conn = getDBconn();
+
+
+    // $sql = "INSERT INTO article (title, content, published_at)
+    //         VALUES ('" . mysqli_escape_string($conn, $_POST['title']) . "','"
+    //                    . mysqli_escape_string($conn, $_POST['content']) . "','"
+    //                    . mysqli_escape_string($conn, $_POST['published_at']) ."')";
+
+    $sql = "INSERT INTO article (title, content, published_at)
+            VALUES (?, ?, ?)";
+
+    $stmt = mysqli_prepare($conn, $sql);
+
+    if ($stmt === false ){
+
+      echo mysqli_error($conn);
+
+      }
+    else {
+      mysqli_stmt_bind_param($stmt, "sss",
+        $title,
+        $content,
+        $published_at);
+
+      if(mysqli_stmt_execute($stmt)) {
+
+        $id = mysqli_insert_id($conn);
+
+        redirect("/Articles/article.php?id=$id");
+
+        }
+      else {
+        echo mysqli_stmt_error($stmt);
+        }
+      }
+    }
+  }
+
+ ?>
+
+<?php require 'includes/header.php' ?>
+
+<h3><a href="Index.php">Main page</a></h3>
+
+<h2>New article</h2>
+<?php require 'includes/article-form.php'; ?>
+
+<?php require 'includes/footer.php' ?>
